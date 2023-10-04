c754ef8fbc4c5d18a3ffbbe8659dd7195adbe8c8
veelatseng <veela.tzeng@gmail.com>
2023-10-04 11:35:10 +0800

    修改menu位置／cp文字折行

--- js/main.js
+++ js/main.js
@@ -1458,7 +1458,7 @@ function categoryActive() {
     const item = i.querySelectorAll('a');
     item.forEach((tag) => {
       tag.addEventListener('click', (e) => {
-        e.preventDefault();
+        // e.preventDefault();
         removeClass(item);
         e.target.classList.add('active');
       });
